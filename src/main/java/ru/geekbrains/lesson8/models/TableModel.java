85e91e91dabfe6857729f29a10761f3e127a73c6
Stanislav <redkorona@yandex.ruggggggggggggggg>
2023-09-21 02:05:03 +0300

    add DZ8

---
+++ src/main/java/ru/geekbrains/lesson8/models/TableModel.java
@@ -0,0 +1,69 @@
+package ru.geekbrains.lesson8.models;
+
+import ru.geekbrains.lesson8.presenters.Model;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+
+public class TableModel implements Model {
+
+
+    private Collection<Table> tables;
+
+    /**
+     * Получение списка всех столиков
+     */
+    public Collection<Table> loadTables(){
+
+        if (tables == null){
+            tables = new ArrayList<>();
+
+            tables.add(new Table());
+            tables.add(new Table());
+            tables.add(new Table());
+            tables.add(new Table());
+            tables.add(new Table());
+        }
+
+        return tables;
+    }
+
+    /**
+     * Бронирование столика
+     * @param reservationDate Дата бронирования
+     * @param tableNo Номер столика
+     * @param name Имя
+     */
+    @Override
+    public int reservationTable(Date reservationDate, int tableNo, String name) {
+        for (Table table: tables) {
+            if (table.getNo() == tableNo){
+                Reservation reservation = new Reservation(table, reservationDate, name);
+                table.getReservations().add(reservation);
+                return reservation.getId();
+            }
+        }
+        throw new RuntimeException("Ошибка бронирования столика. Повторите попытку позже.");
+    }
+
+    /**
+     * TODO: Доработать самостоятельнов рамках домашней работы
+     * @param oldReservation идентификатор бронирования (старый)
+     * @param reservationDate дата бронирования
+     * @param tableNo номер столика
+     * @param name Имя
+     * @return
+     */
+    public int changeReservationTable(int oldReservation, Date reservationDate, int tableNo, String name){
+        for (Table table: tables) {
+            for (Reservation reservation: table.getReservations()){
+                if (reservation.getId() == oldReservation && tables.size() >= tableNo){
+                    table.getReservations().remove(reservation);
+                    return reservationTable(reservationDate, tableNo, name);
+                }
+            }
+        }
+        throw new RuntimeException("Ошибка изменения бронирования столика. Повторите попытку позже.");
+    }
+}
